ee248c7a6346b656966ea6ecae1c14e20df3f318
Arpit_Jaiswal <jaiswalarpit1414@gmail.com>
2024-07-09 12:45:14 +0530

    Setup frontend with tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import Dashboard from './Components/Dashboards'
+
+function App() {
+
+  return (
+    <div className="min-h-screen bg-gray-100 p-6">
+      <Dashboard />
+    </div>
+  )
+}
+
+export default App
